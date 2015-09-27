393c564a5bb549afd3729cda7bddf14a2f915877
Gerard Briones <brionesgc@vcu.edu>
2015-09-26 20:51:54 -0400

    working on form fragment

--- app/src/main/java/hackdc/safetynet/API/FormFragment.java
+++ app/src/main/java/hackdc/safetynet/API/FormFragment.java
@@ -1,7 +1,33 @@
 package hackdc.safetynet.API;
 
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import hackdc.safetynet.R;
+
 /**
  * Created by illipino_g on 9/26/15.
  */
-public class FormFragment {
+public class FormFragment extends Fragment {
+
+    public static FormFragment newInstance() {
+        FormFragment fragment = new FormFragment();
+
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater layoutInflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = layoutInflater.inflate(R.layout.fragment_form, container, false);
+
+        return view;
+    }
 }
